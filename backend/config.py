5d03134e3171be7751a4ea3ed938dea982e4a836
VitorioSilva <luuckdev@proton.me>
2024-11-11 13:33:04 -0300

    organizei o js

--- backend/config.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-def configure_app(app):
-    app.secret_key = os.getenv("SECRET_KEY")
-    app.config["JWT_SECRET"] = os.getenv("JWT_SECRET")
-    app.config["JWT_ALGORITHM"] = "HS256"
--- backend/utils/validators.py
+++
@@ -1,5 +0,0 @@
-import re
-
-def validar_email(email):
-    email_formato = r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$"
-    return re.match(email_formato, email)
